557d69ceaf1ae0f0898c5fd8b01f016095cb3e78
Devesh <rockdev1234560@gmail.com>
2020-12-03 01:32:12 +0530

    anon signinout complete

--- home/home.dart
+++ home/home.dart
@@ -1,13 +1,29 @@
+import 'package:crewbrew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:crewbrew/services/auth.dart';
 
 class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home',
-      style: TextStyle(
-        fontSize: 80.0,
-      ),),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('logout'),
+            onPressed:() async{
+             await _auth.signOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- wrapper.dart
+++ wrapper.dart
@@ -1,13 +1,23 @@
 import 'package:crewbrew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:crewbrew/screens/home/home.dart';
+import 'package:provider/provider.dart';
+import 'package:crewbrew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+    print(user);
+
     //return either home or authenticate
-    return Authenticate();
+    if(user==null){
+      return Authenticate();
+    }else{
+      return Home();
+    }
+
   }
 }
 
